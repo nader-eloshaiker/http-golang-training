91b9f16c7196a07191d364d7ad5af511f1ff47ed
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2018-12-04 17:07:21 +1100

    Section 6 Lecture 64 - http interfaces

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package http_golang_training
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Print("error:", err)
+		os.Exit(1)
+	}
+
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+
+	fmt.Println(string(bs))
+}
